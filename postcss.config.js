78ffb9f61ea58a67b5d152a8532317e84c67ff4e
Georgi Varbanov <support@atompillar.com>
2025-06-14 15:51:53 +0100

    🧹 Resolved leftover merge conflict

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
-<<<<<<< HEAD
+
 module.exports = {
-=======
+
 export default {
->>>>>>> 05eca8c668072af566a6048713108509ce438908
+05eca8c668072af566a6048713108509ce438908
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
